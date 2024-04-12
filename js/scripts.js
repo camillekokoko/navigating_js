78a9199dd2d7403db3f7037fb02ad187c7c5e165
Camille Ko <camille.ko@adelaide.edu.au>
2024-04-12 14:28:01 +0930

    lecture2

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,31 @@ codingBoss();
 console.log("Hello World");
 
 console.log(2 + 5);
+
+//Assignment 2 : VARIABLES AND DATA TYPES
+let customerName = "Camille";
+let coffeeName = "Flat White";
+let coffeePrice = 4.5;
+let isCoffeeHot = true;
+
+
+function serveCoffee() {
+    let sugar = true;
+    let size = "Medium";
+    let shot = 1;
+    let isDecaf = false;
+    console.log("Preparing", size, shot, coffeeName, isDecaf);
+    console.log("Adding sugar:", sugar);
+    console.log("Serving coffee to the customer...");
+}
+
+console.log("Customer Name:", customerName.toUpperCase());
+console.log("Coffee Name:", coffeeName);
+console.log("Coffee Price:", coffeePrice);
+console.log("Is Coffee Hot:", isCoffeeHot);
+
+serveCoffee();
+
+console.clear();
+
+console.warn(`${customerName}, your coffee is ready`);
